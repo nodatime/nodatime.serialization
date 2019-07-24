32f39edafca17371c70eff9be3cda08b63065baa
Dzmitry Safarau <DSilencea@gmail.com>
2019-07-24 23:16:56 +0300

    Added: Initial System.Text.Json serialization support

---
+++ src/NodaTime.Serialization.SystemText/Preconditions.cs
@@ -0,0 +1,35 @@
+// Copyright 2017 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using NodaTime.Utility;
+
+namespace NodaTime.Serialization.SystemText
+{
+    /// <summary>
+    /// Helper static methods for argument/state validation. (Just the subset used within this library.)
+    /// </summary>
+    internal static class Preconditions
+    {
+        internal static T CheckNotNull<T>(T argument, string paramName)
+            => argument == null ? throw new ArgumentNullException(paramName) : argument;
+
+        internal static void CheckArgument(bool expression, string parameter, string message)
+        {
+            if (!expression)
+            {
+                throw new ArgumentException(message, parameter);
+            }
+        }
+
+        internal static void CheckData<T>(bool expression, string messageFormat, T messageArg)
+        {
+            if (!expression)
+            {
+                string message = string.Format(messageFormat, messageArg);
+                throw new InvalidNodaDataException(message);
+            }
+        }
+    }
+}
