7f64ee266af5fa1a716d71a706df366d77ff26e3
Dzmitry Safarau <DSilencea@gmail.com>
2019-07-26 18:04:45 +0300

    Finished porting tests.

--- src/NodaTime.Serialization.Test/SystemText/NodaDateTimeZoneConverterTest.cs
+++ src/NodaTime.Serialization.Test/SystemText/NodaDateTimeZoneConverterTest.cs
@@ -1,23 +1,31 @@
-// Copyright 2012 The Noda Time Authors. All rights reserved.
+// Copyright 2019 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
-using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using NodaTime.Serialization.SystemText;
 using NodaTime.TimeZones;
 using NUnit.Framework;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace NodaTime.Serialization.Test.SystemText
 {
     public class NodaDateTimeZoneConverterTest
     {
-        private readonly JsonConverter converter = NodaConverters.CreateDateTimeZoneConverter(DateTimeZoneProviders.Tzdb);
+        private readonly JsonConverter<DateTimeZone> converter =
+            NodaConverters.CreateDateTimeZoneConverter(DateTimeZoneProviders.Tzdb);
 
         [Test]
         public void Serialize()
         {
             var dateTimeZone = DateTimeZoneProviders.Tzdb["America/Los_Angeles"];
-            var json = JsonConvert.SerializeObject(dateTimeZone, Formatting.None, converter);
+            var json = JsonSerializer.Serialize(dateTimeZone, new JsonSerializerOptions
+            {
+                WriteIndented = false,
+                Converters = {converter}
+            });
             string expectedJson = "\"America/Los_Angeles\"";
             Assert.AreEqual(expectedJson, json);
         }
@@ -26,7 +34,10 @@ namespace NodaTime.Serialization.Test.SystemText
         public void Deserialize()
         {
             string json = "\"America/Los_Angeles\"";
-            var dateTimeZone = JsonConvert.DeserializeObject<DateTimeZone>(json, converter);
+            var dateTimeZone = JsonSerializer.Deserialize<DateTimeZone>(json, new JsonSerializerOptions
+            {
+                Converters = {converter}
+            });
             var expectedDateTimeZone = DateTimeZoneProviders.Tzdb["America/Los_Angeles"];
             Assert.AreEqual(expectedDateTimeZone, dateTimeZone);
         }
@@ -35,7 +46,12 @@ namespace NodaTime.Serialization.Test.SystemText
         public void Deserialize_TimeZoneNotFound()
         {
             string json = "\"America/DOES_NOT_EXIST\"";
-            var exception = Assert.Throws<JsonSerializationException>(() => JsonConvert.DeserializeObject<DateTimeZone>(json, converter));
+            var exception =
+                Assert.Throws<SerializationException>(() =>
+                    JsonSerializer.Deserialize<DateTimeZone>(json, new JsonSerializerOptions
+                    {
+                        Converters = {converter}
+                    }));
             Assert.IsInstanceOf<DateTimeZoneNotFoundException>(exception.InnerException);
         }
     }
