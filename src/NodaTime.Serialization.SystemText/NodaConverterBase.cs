4b349dc715359a092448b01135804918995f7590
Dzmitry Safarau <DSilencea@gmail.com>
2019-07-26 18:04:45 +0300

    Changed to base converter. Some refactoring.

--- src/NodaTime.Serialization.SystemText/NodaConverterBase.cs
+++ src/NodaTime.Serialization.SystemText/NodaConverterBase.cs
@@ -19,30 +19,6 @@ namespace NodaTime.Serialization.SystemText
     public abstract class NodaConverterBase<T> : JsonConverter<T>
     {
         /// <summary>
-        /// Default constructor.
-        /// </summary>
-        protected NodaConverterBase()
-        {
-        }
-
-        // For value types and sealed classes, we can optimize and not call IsAssignableFrom.
-        private static readonly bool CheckAssignableFrom =
-            !(typeof(T).GetTypeInfo().IsValueType || (typeof(T).GetTypeInfo().IsClass && typeof(T).GetTypeInfo().IsSealed));
-
-        private static readonly Type NullableT = typeof(T).GetTypeInfo().IsValueType
-            ? typeof(Nullable<>).MakeGenericType(typeof(T)) : typeof(T);
-
-        /// <summary>
-        /// Returns whether or not this converter supports the given type.
-        /// </summary>
-        /// <param name="objectType">The type to check for compatibility.</param>
-        /// <returns>True if the given type is supported by this converter (including the nullable form for
-        /// value types); false otherwise.</returns>
-        public override bool CanConvert(Type objectType) =>
-            objectType == typeof(T) || objectType == NullableT ||
-            (CheckAssignableFrom && typeof(T).GetTypeInfo().IsAssignableFrom(objectType.GetTypeInfo()));
-
-        /// <summary>
         /// Converts the JSON stored in a reader into the relevant Noda Time type.
         /// </summary>
         /// <param name="reader">The json reader to read data from.</param>
@@ -52,39 +28,15 @@ namespace NodaTime.Serialization.SystemText
         /// <returns>The deserialized value.</returns>
         public override T Read(ref Utf8JsonReader reader, Type objectType, JsonSerializerOptions options)
         {
-            if (reader.TokenType == JsonTokenType.Null)
-            {
-                Preconditions.CheckData(objectType == NullableT,
-                    "Cannot convert null value to {0}",
-                    objectType);
-                return default;
-            }
-
-            // Handle empty strings automatically
-            if (reader.TokenType == JsonTokenType.String)
-            {
-                string value = reader.GetString();
-                if (value == "")
-                {
-                    Preconditions.CheckData(objectType == NullableT,
-                        "Cannot convert null value to {0}",
-                        objectType);
-                    return default;
-                }
-            }
-
             try
             {
-                // Delegate to the concrete subclass. At this point we know that we don't want to return null, so we
-                // can ask the subclass to return a T, which we will box. That will be valid even if objectType is
-                // T? because the boxed form of a non-null T? value is just the boxed value itself.
-
+                // Delegate to the concrete subclass.
                 // Note that we don't currently pass existingValue down; we could change this if we ever found a use for it.
                 return ReadJsonImpl(ref reader, options);
             }
             catch (Exception ex)
             {
-                throw new SerializationException($"Cannot convert value to {objectType}", ex);
+                throw new JsonException($"Cannot convert value to {objectType}", ex);
             }
         }
 
@@ -106,8 +58,6 @@ namespace NodaTime.Serialization.SystemText
         public override void Write(Utf8JsonWriter writer,
             T value, JsonSerializerOptions options)
         {
-            // Json.NET should prevent this happening, but let's validate...
-            Preconditions.CheckNotNull(value, nameof(value));
             WriteJsonImpl(writer, value, options);
         }
 
