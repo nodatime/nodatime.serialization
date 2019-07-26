ce37864366469d9eab858269f910c0af229471e7
Dzmitry Safarau <DSilencea@gmail.com>
2019-07-26 14:52:41 +0300

    Cleanup code, started some tests.

--- src/NodaTime.Serialization.SystemText/NodaDateIntervalConverter.cs
+++ src/NodaTime.Serialization.SystemText/NodaDateIntervalConverter.cs
@@ -1,4 +1,4 @@
-// Copyright 2017 The Noda Time Authors. All rights reserved.
+// Copyright 2019 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
@@ -8,7 +8,7 @@ using NodaTime.Utility;
 namespace NodaTime.Serialization.SystemText
 {
     /// <summary>
-    /// Json.NET converter for <see cref="DateInterval"/> using a compound representation. The start and
+    /// System.Text.Json converter for <see cref="DateInterval"/> using a compound representation. The start and
     /// end aspects of the date interval are represented with separate properties, each parsed and formatted
     /// by the <see cref="LocalDate"/> converter for the serializer provided.
     /// </summary>
@@ -19,7 +19,7 @@ namespace NodaTime.Serialization.SystemText
         /// using the given serializer.
         /// </summary>
         /// <param name="reader">The JSON reader to fetch data from.</param>
-        /// <param name="serializer">The serializer for embedded serialization.</param>
+        /// <param name="options">The serializer options for embedded serialization.</param>
         /// <returns>The <see cref="DateInterval"/> identified in the JSON.</returns>
         protected override DateInterval ReadJsonImpl(ref Utf8JsonReader reader, JsonSerializerOptions options)
         {
@@ -69,7 +69,7 @@ namespace NodaTime.Serialization.SystemText
         /// </summary>
         /// <param name="writer">The writer to write JSON to</param>
         /// <param name="value">The date interval to serialize</param>
-        /// <param name="serializer">The serializer for embedded serialization.</param>
+        /// <param name="options">The serializer options for embedded serialization.</param>
         protected override void WriteJsonImpl(Utf8JsonWriter writer, DateInterval value, JsonSerializerOptions options)
         {
             writer.WriteStartObject();
---
+++ src/NodaTime.Serialization.Test/SystemText/DelegatingConverterBaseTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2019 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NodaTime.Serialization.SystemText;
+using NodaTime.Text;
+using NUnit.Framework;
+using NodaConverters = NodaTime.Serialization.SystemText.NodaConverters;
+
+namespace NodaTime.Serialization.Test.SystemText
+{
+    public class DelegatingConverterBaseTest
+    {
+        [Test]
+        public void Serialize()
+        {
+            string expected = "{'ShortDate':'2017-02-20','LongDate':'20 February 2017'}"
+                .Replace("'", "\"");
+            var date = new LocalDate(2017, 2, 20);
+            var entity = new Entity { ShortDate = date, LongDate = date };
+            var actual = JsonSerializer.Serialize(entity, new JsonSerializerOptions
+            {
+                WriteIndented = false
+            });
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Deserialize()
+        {
+            string json = "{'ShortDate':'2017-02-20','LongDate':'20 February 2017'}"
+                .Replace("'", "\"");
+            var expectedDate = new LocalDate(2017, 2, 20);
+            var entity = JsonSerializer.Deserialize<Entity>(json);
+            Assert.AreEqual(expectedDate, entity.ShortDate);
+            Assert.AreEqual(expectedDate, entity.LongDate);
+        }
+
+        public class Entity
+        {
+            [JsonConverter(typeof(ShortDateConverter))]
+            public LocalDate ShortDate { get; set; }
+
+            [JsonConverter(typeof(LongDateConverter))]
+            public LocalDate LongDate { get; set; }
+        }
+
+        public class ShortDateConverter : DelegatingConverterBase<LocalDate>
+        {
+            public ShortDateConverter() : base(NodaConverters.LocalDateConverter) { }
+        }
+
+        public class LongDateConverter : DelegatingConverterBase<LocalDate>
+        {
+            // No need to create a new one of these each time...
+            private static readonly JsonConverter<LocalDate> converter =
+                new Serialization.SystemText.NodaPatternConverter<LocalDate>(LocalDatePattern.CreateWithInvariantCulture("d MMMM yyyy"));
+
+            public LongDateConverter() : base(converter)
+            {
+            }
+        }
+    }
+}
