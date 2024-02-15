e034da369240dc26656159f2a09837ece3f381f3
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2024-02-15 17:04:36 -0600

    Add dictionary key support for STJ

--- src/NodaTime.Serialization.SystemTextJson/NodaConverterBase.cs
+++ src/NodaTime.Serialization.SystemTextJson/NodaConverterBase.cs
@@ -64,6 +64,30 @@ namespace NodaTime.Serialization.SystemTextJson
             }
         }
 
+#if NET6_0_OR_GREATER
+        /// <summary>
+        /// Converts the JSON stored in a reader into the relevant Noda Time type.
+        /// </summary>
+        /// <param name="reader">The json reader to read data from.</param>
+        /// <param name="typeToConvert">The type to convert the JSON to.</param>
+        /// <param name="options">A serializer options to use for any embedded deserialization.</param>
+        /// <exception cref="InvalidNodaDataException">The JSON was invalid for this converter.</exception>
+        /// <returns>The deserialized value.</returns>
+        public override T ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert,
+            JsonSerializerOptions options)
+        {
+            try
+            {
+                // Delegate to the concrete subclass.
+                return ReadJsonImpl(ref reader, options);
+            }
+            catch (Exception ex)
+            {
+                throw new JsonException($"Cannot convert value to {typeToConvert}", ex);
+            }
+        }
+#endif
+
         /// <summary>
         /// Implemented by concrete subclasses, this performs the final conversion from a non-null JSON value to
         /// a value of type T.
@@ -86,6 +110,17 @@ namespace NodaTime.Serialization.SystemTextJson
         public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) =>
             WriteJsonImpl(writer, value, options);
 
+#if NET6_0_OR_GREATER
+        /// <summary>
+        /// Writes the value as a string to a Utf8JsonWriter.
+        /// </summary>
+        /// <param name="writer">The writer to write the JSON to.</param>
+        /// <param name="value">The value to write.</param>
+        /// <param name="options">The serializer options to use for any embedded serialization.</param>
+        public override void WriteAsPropertyName(Utf8JsonWriter writer, T value, JsonSerializerOptions options) =>
+            WriteJsonImpl(writer, value, options, true);
+#endif
+
         /// <summary>
         /// Implemented by concrete subclasses, this performs the final write operation for a non-null value of type T
         /// to JSON.
@@ -93,6 +128,7 @@ namespace NodaTime.Serialization.SystemTextJson
         /// <param name="writer">The writer to write JSON data to</param>
         /// <param name="value">The value to serializer</param>
         /// <param name="options">The serializer options to use for nested serialization</param>
-        protected abstract void WriteJsonImpl(Utf8JsonWriter writer, T value, JsonSerializerOptions options);
+        /// <param name="isProperty">Boolean flag to determine which writer method to invoke</param>
+        protected abstract void WriteJsonImpl(Utf8JsonWriter writer, T value, JsonSerializerOptions options, bool isProperty = false);
     }
 }
--- src/NodaTime.Serialization.SystemTextJson/NodaIsoIntervalConverter.cs
+++ src/NodaTime.Serialization.SystemTextJson/NodaIsoIntervalConverter.cs
@@ -49,11 +49,17 @@ namespace NodaTime.Serialization.SystemTextJson
         /// <param name="writer">The writer to write JSON to</param>
         /// <param name="value">The interval to serialize</param>
         /// <param name="options">The serializer options for embedded serialization.</param>
-        protected override void WriteJsonImpl(Utf8JsonWriter writer, Interval value, JsonSerializerOptions options)
+        /// <param name="isProperty">Interval cannot be converted to a scalar string value as such it is not a valid choice for a dictionary key</param>
+        protected override void WriteJsonImpl(Utf8JsonWriter writer, Interval value, JsonSerializerOptions options, bool isProperty = false)
         {
             var pattern = InstantPattern.ExtendedIso;
-            string text = (value.HasStart ? pattern.Format(value.Start) : "") + "/" + (value.HasEnd ? pattern.Format(value.End) : "");
-            writer.WriteStringValue(text);
+            var text = $"{(value.HasStart ? pattern.Format(value.Start) : "")}/{(value.HasEnd ? pattern.Format(value.End) : "")}";
+#if NET6_0_OR_GREATER
+            if (isProperty)
+                writer.WritePropertyName(text);
+            else
+#endif
+                writer.WriteStringValue(text);
         }
     }
 }
