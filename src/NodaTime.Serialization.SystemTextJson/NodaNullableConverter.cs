4ee939c656e6df3bafddec36667bde3114d1d950
Brant Burnett <bburnett@centeredgesoftware.com>
2024-10-29 16:52:54 -0400

    Sealed and added internal XML doc comments

--- src/NodaTime.Serialization.SystemTextJson/NodaNullableConverter.cs
+++ src/NodaTime.Serialization.SystemTextJson/NodaNullableConverter.cs
@@ -8,10 +8,19 @@ using System.Text.Json.Serialization;
 
 namespace NodaTime.Serialization.SystemTextJson;
 
-internal class NodaNullableConverter<T> : JsonConverter<T?> where T : struct
+/// <summary>
+/// System.Text.Json converter for <see cref="Nullable{T}"/> value types, wrapping
+/// an inner converter.
+/// </summary>
+/// <typeparam name="T">Value type to be converted.</typeparam>
+internal sealed class NodaNullableConverter<T> : JsonConverter<T?> where T : struct
 {
     private readonly JsonConverter<T> _innerConverter;
 
+    /// <summary>
+    /// Creates a new NodaNullableConverter.
+    /// </summary>
+    /// <param name="innerConverter">Inner converter for serializing and deserializing when not null.</param>
     public NodaNullableConverter(JsonConverter<T> innerConverter)
     {
         Preconditions.CheckNotNull(innerConverter, nameof(innerConverter));
@@ -19,6 +28,7 @@ internal class NodaNullableConverter<T> : JsonConverter<T?> where T : struct
         _innerConverter = innerConverter;
     }
 
+    /// <inheritdoc />
     public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.Null)
@@ -29,6 +39,7 @@ internal class NodaNullableConverter<T> : JsonConverter<T?> where T : struct
         return _innerConverter.Read(ref reader, typeToConvert, options);
     }
 
+    /// <inheritdoc />
     public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
     {
         if (value is null)
